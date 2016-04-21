0961ad1fe6f02ccbc7488dec1add0bc6c6c0d146
Jeff Jones <spjeff@spjeff.com>
2016-04-21 10:47:35 -0500

    Code
    * regions
    * remove remote EXE clean up
    * enable CredSPP client
    * force IIS start at end (HTTP 404)
    * custom transcript TXT name

--- SPPatchify.ps1
+++ SPPatchify.ps1
@@ -1,8 +1,9 @@
 # Plugin
 Add-PSSnapIn Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue | Out-Null
 
-Function CopyEXE() {
-	Write-Host "===== CopyEXE =====" -Fore Yellow
+#region binary EXE
+Function CopyEXE($action) {
+	Write-Host "===== $action EXE =====" -Fore Yellow
 
 	# Loop servers
 	$counter = 0
@@ -14,14 +15,21 @@ Function CopyEXE() {
 		
 		# Skip current machine
 		if ($addr -ne $env:computername) {
-			# Copy
-			$files = Get-ChildItem ".\media\*.*"
-			foreach ($file in $files) {
-				$name = $file.Name
-				$dest = "\\$addr\C$\SPPatchify\media"
-				mkdir $dest -Force -ErrorAction SilentlyContinue | Out-Null
-				ROBOCOPY "media" $dest /Z /W:0 /R:0 /XX
+			if ($action -eq "Copy") {
+				# Copy
+				$files = Get-ChildItem ".\media\*.*"
+				foreach ($file in $files) {
+					$name = $file.Name
+					$dest = "\\$addr\C$\SPPatchify\media"
+					mkdir $dest -Force -ErrorAction SilentlyContinue | Out-Null
+					mkdir $dest.replace("media","log") -Force -ErrorAction SilentlyContinue | Out-Null
+					ROBOCOPY "media" $dest /Z /W:0 /R:0 /XX
+				}
+			} else {
+				# Delete
+				del "\\$addr\C$\SPPatchify\media\*.*" -confirm:$false
 			}
+			
 		}
 	}
 	Write-Progress -Activity "Completed" -Completed
@@ -37,8 +45,35 @@ Function StartEXE() {
 	} else {
 		$name = $files.Name
 	}
-	$cmd = "C:\SPPatchify\media\$name /quiet /passive /forcerestart /log:""C:\SPPatchify\$name.log"""
+	$global:patchName = $name.replace(".exe","")
+	$cmd = "Start-Process 'C:\SPPatchify\media\$name' -ArgumentList '/quiet /forcerestart /log:""C:\SPPatchify\log\$name.log""' -PassThru -NoNewWindow"
 	LoopRemoteCmd "Run EXE on " $cmd
+}
+
+Function WaitEXE() {
+	Write-Host "===== WaitEXE =====" -Fore Yellow
+	
+	# Wait for reboot
+	Write-Host "Wait 30 sec..."
+	Sleep 30
+			
+	# Verify machines online
+	$counter = 0
+	foreach ($server in $servers) {
+		# Progress
+		$addr = $server.Address
+		Write-Progress -Activity "Waiting for " -Status $addr -PercentComplete (($counter/$servers.Count)*100)
+		$counter++
+		
+		# Remote Posh
+		$when = Get-Date
+		Write-Host "`nEXE started on $addr at $when " -NoNewLine
+		do {
+			$proc = Get-Process -Name $global:patchName -Computer $addr -ErrorAction SilentlyContinue
+			Sleep 2
+			Write-Host "."  -NoNewLine
+		} while ($proc)
+	}
 }
 
 Function WaitReboot() {
@@ -63,7 +98,9 @@ Function WaitReboot() {
 	}
 	Get-PSSession | Remove-PSSession
 }
+#endregion
 
+#region SP Config Wizard
 Function LoopRemoteCmd($msg, $cmd) {
 	# Loop servers
 	$counter = 0
@@ -71,9 +108,11 @@ Function LoopRemoteCmd($msg, $cmd) {
 		# Script block
 		if ($cmd.GetType().Name -eq "String") {
 			if ($env:computername -eq $server.Address) {
-				$cmd = $cmd -replace "forcerestart","norestart"
+				$runCmd = $cmd -replace "forcerestart","norestart"
+			} else {
+				$runCmd = $cmd
 			}
-			$sb = [ScriptBlock]::Create("$cmd")
+			$sb = [ScriptBlock]::Create("$runCmd")
 		} else {
 			$sb = $cmd
 		}
@@ -87,8 +126,10 @@ Function LoopRemoteCmd($msg, $cmd) {
 		# Remote Posh
 		$remote = New-PSSession -ComputerName $addr -Authentication CredSSP -Credential $global:cred
 		Write-Host ">> invoke on $addr" -Fore Green
-		$sb.ToString()
-		Invoke-Command -Session $remote -ScriptBlock $sb
+		foreach ($s in $sb) {
+			$s.ToString()
+			Invoke-Command -Session $remote -ScriptBlock $s
+		}
 		Write-Host "<< complete on $addr" -Fore Green
 	}
 	Get-PSSession | Remove-PSSession
@@ -132,7 +173,7 @@ Function ChangeServices($state) {
 	# Logic core
 	if ($state) {
 		$action = "START"
-		$scriptBlock = {
+		$sb = {
 			@("IISAdmin","SPTimerV4") |% {
 				if (Get-Service $_) {
 					Set-Service -Name $_ -StartupType Automatic -ErrorAction SilentlyContinue
@@ -146,7 +187,7 @@ Function ChangeServices($state) {
 		}
 	} else {
 		$action = "STOP"
-		$scriptBlock = {
+		$sb = {
 			Start-Process 'iisreset.exe' -ArgumentList '/stop' -Wait -PassThru -NoNewWindow | Out-Null
 			@("IISAdmin","SPTimerV4") |% {
 				if (Get-Service $_) {
@@ -171,14 +212,32 @@ Function ChangeServices($state) {
 		}
 	} catch {}
 	
-	LoopRemoteCmd "$action services on " $scriptBlock
+	LoopRemoteCmd "$action services on " $sb
 }
 
 Function RunConfigWizard() {
-	# Build CMD
-	$ver = (Get-SPFarm).BuildVersion.Major;
-	$configWizard = "& 'C:\Program Files\Common Files\microsoft shared\Web Server Extensions\$ver\BIN\psconfig.exe' -cmd upgrade -inplace b2b -wait"
-	LoopRemoteCmd "Run Config Wizard on " $configWizard
+	# Shared
+	$shared = {
+		Add-PSSnapIn Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue | Out-Null
+		$ver = (Get-SPFarm).BuildVersion.Major;
+		$psconfig = "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\$ver\BIN\psconfig.exe"
+		$options = "-cmd upgrade -inplace b2b -wait -cmd applicationcontent -install -cmd installfeatures -cmd secureresources"
+	}
+	
+	# Save B2B shortcut
+	$b2b = {
+		$file = $psconfig.replace("psconfig.exe","psconfigb2b.cmd")
+		if (!(Test-Path $file)) {
+			"psconfig.exe $options" | Out-File $file -Force
+		}
+	}
+	LoopRemoteCmd "Save B2B shortcut on " @($shared,$b2b)
+	
+	# Run Config Wizard
+	$wiz = {
+		& "$psconfig" $options
+	}
+	LoopRemoteCmd "Run Config Wizard on " @($shared,$wiz)
 }
 
 Function ChangeContent($state) {
@@ -209,8 +268,19 @@ Function ChangeContent($state) {
 			Write-Progress -Activity "Add database" -Status "$name ($prct %)" -PercentComplete $prct
 			$counter++
 		
-			Mount-SPContentDatabase -WebApplication $_."`$wa" -Name $name -DatabaseServer $_.NormalizedDataSource
+			Mount-SPContentDatabase -WebApplication $_."`$wa" -Name $name -DatabaseServer $_.NormalizedDataSource | Out-Null
 		}
+	}
+}
+#endregion
+
+#region general
+Function EnablePS() {
+	$ssp = Get-WSManCredSSP
+	if ($ssp[0] -eq "The machine is not configured to allow delegating fresh credentials.") {
+		# Enable remote PowerShell over CredSSP authentication
+		Enable-WSManCredSSP -DelegateComputer * -Role Client -Force
+		Restart-Service WinRM
 	}
 }
 
@@ -243,26 +313,59 @@ Function ReadIISPW {
 	$global:cred = New-Object System.Management.Automation.PSCredential -ArgumentList "$domain\$user",$sec
 }
 
+Function DisplayCA() {
+	# version table
+	(Get-SPFarm).BuildVersion
+	$ver = (Get-SPFarm).BuildVersion.Major;
+	
+	$sb = {
+		[System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\Common Files\microsoft shared\Web Server Extensions\$ver\ISAPI\Microsoft.SharePoint.dll") | select FileVersion,@{N='PC'; E={$env:computername}}
+	}
+	LoopRemoteCmd "Get file version on " $sb
+	
+	# open Central Admin
+	$ca = (Get-SPWebApplication -IncludeCentralAdministration) |? {$_.IsAdministrationWebApplication}
+	$pages = @("PatchStatus.aspx","UpgradeStatus.aspx","FarmServers.aspx")
+	$pages |% {start ($ca.Url + "_admin/" + $_)}
+}
+
+Function IISStart() {
+	$sb = {
+		net start w3svc
+		Get-ChildItem IIS:\AppPools |% {$n=$_.Name; Start-WebAppPool $n}
+		Get-WebSite | Start-WebSite
+	}
+	LoopRemoteCmd "Start IIS on " $sb
+}
+#endregion
+
 Function Main() {
 	# Start time
-	Start-Transcript
 	$start = Get-Date
+	$when = $start.ToString("yyyy-MM-dd-hh-mm-ss")
+	$logFile = "log\$($MyInvocation.MyCommand)-$when.txt"
+	Start-Transcript $logFile
 
-	# Local farm scope
+	# Local farm
+	(Get-SPFarm).BuildVersion
 	$servers = Get-SPServer |? {$_.Role -ne "Invalid"}
-	$when = (Get-Date).ToString("yyyy-MM-dd-hh-mm-ss")
 	
 	# Core steps
+	EnablePS
  	ReadIISPW
-	CopyEXE
+	CopyEXE "Copy"
 	ChangeDC
 	ChangeServices $false
 	StartEXE
+	WaitEXE
 	WaitReboot
 	ChangeContent $false
 	ChangeServices $true
 	RunConfigWizard
 	ChangeContent $true
+	CopyEXE "Remove"
+	IISStart
+	DisplayCA
 	
 	# Run duration
 	Write-Host "===== DONE =====" -Fore Yellow
