9c5e987bfd2957e3818571629d868ca81b71bc7c
Jeff Jones <spjeff@spjeff.com>
2018-06-27 11:37:49 -0500

    Code
     SPTimer

--- SPPatchify.ps1
+++ SPPatchify.ps1
@@ -10,7 +10,7 @@
 .NOTES
 	File Namespace	: SPPatchify.ps1
 	Author			: Jeff Jones - @spjeff
-	Version			: 0.104
+	Version			: 0.105
 	Last Modified	: 06-27-2018
 .LINK
 	Source Code
@@ -1439,22 +1439,23 @@ function PreflightCheck() {
     }
 }
 
-function ClearCacheIni() {
+function ClearCacheIni() {  
+
     Write-Host "Clear CACHE.INI " -Fore Green
     # Get the local farm instance
-    $farm = Get-SPServer |Where-Object {($_.Role -ne "Invalid")}
+    $farm = Get-SPServer | Where-Object {($_.Role -ne "Invalid")}
 
     # Stop the SharePoint Timer Service on each server in the farm
-    StopSharePointTimerServicesInFarm $farm
+    ChangeSPTimer $farm $false
 
     # Delete all xml files from cache config folder on each server in the farm
-    DeleteXmlFilesFromConfigCache $farm
+    DeleteXmlCache $farm
 
     # Clear the timer cache on each server in the farm
     ClearTimerCache $farm
 
     # Start the SharePoint Timer Service on each server in the farm
-    StartSharePointTimerServicesInFarm $farm
+    ChangeSPTimer $farm $true
     Write-Host "Succeess" -Fore Green
 }
 
@@ -1462,106 +1463,66 @@ function LaunchPhaseThree() {
     Start-Process "powershell.exe" -ArgumentList "$scriptFile -phaseThree"
 }
 
-#**************************************************************************************
-# Constants
-#**************************************************************************************
-Set-Variable timerServiceName -option Constant -value "SPTimerV4"
-Set-Variable timerServiceInstanceName -option Constant -value "Microsoft SharePoint Foundation Timer"
 
-#**************************************************************************************
-# Functions
-#**************************************************************************************
-#<summary>
 # Stops the SharePoint Timer Service on each server in the SharePoint Farm.
-#</summary>
-#<param name="$farm">The SharePoint farm object.</param>
-function StopSharePointTimerServicesInFarm($farm) {
-    Write-Host ""
+function ChangeSPTimer($farm, $state) {
+    # Constants
+    $timerService = "SPTimerV4"
+    $timerInstance = "Microsoft SharePoint Foundation Timer"  
 
     # Iterate through each server in the farm, and each service in each server
     foreach ($server in $farm) {
         foreach ($instance in $server.ServiceInstances) {
             # If the server has the timer service then stop the service
-            if ($instance.TypeName -eq $timerServiceInstanceName) {
-                [string]$serverName = $server.Name
+            if ($instance.TypeName -eq $timerInstance) {
+                # Display
+                $addr = $server.Address
+                if ($state) {
+                    $change = "Started"
+                } else {
+                    $change = "Stopped"
+                }
 
-                Write-Host -foregroundcolor DarkGray -NoNewline "Stop '$timerServiceName' service on server: "
-                Write-Host -foregroundcolor Gray $serverName
+                Write-Host -Foregroundcolor DarkGray -NoNewline "$timerServiceName service on server: "
+                Write-Host -Foregroundcolor Gray $serverName
 
-                $service = Get-WmiObject -ComputerName $serverName Win32_Service -Filter "Name='$timerServiceName'"
-                sc.exe \\$serverName stop $timerServiceName > $null
+                # Change
+                $svc = Get-Service -ComputerName $addr -Name $timerService
+                $svc | Set-Service -StartupType Automatic
+                if ($state) {
+                    $svc.Start()
+                } else {
+                    $svc.Stop()
+                }
 
-                # Wait until this service has actually stopped
-                WaitForServiceState $serverName $timerServiceName "Stopped"
-
+                # Wait for service stop/start
+                WaitSPTimer $serverName $timerServiceName $change
                 break;
             }
         }
     }
-
-    Write-Host ""
 }
 
-#<summary>
+
 # Waits for the service on the server to reach the required service state.
-# This can be used to wait for the "SharePoint 2010 Timer" service to stop or to start
-#</summary>
-#<param name="$serverName">The name of the server with the service to monitor.</param>
-#<param name="$serviceName">The name of the service to monitor.</param>
-#<param name="$serviceState">The service state to wait for, e.g. Stopped, or Running.</param>
-function WaitForServiceState([string]$serverName, [string]$serviceName, [string]$serviceState) {
-    Write-Host -foregroundcolor DarkGray -NoNewLine "Waiting for service '$serviceName' to change state to $serviceState on server $serverName"
+function WaitSPTimer($addr, $service, $state) {
+    Write-Host -foregroundcolor DarkGray -NoNewLine "Waiting for service '$service' to change state to $state on server $addr"
 
     do {
         Start-Sleep 1
-        Write-Host -foregroundcolor DarkGray -NoNewLine "."
-        $service = Get-WmiObject -ComputerName $serverName Win32_Service -Filter "Name='$serviceName'"
+        Write-Host -Foregroundcolor DarkGray -NoNewLine "."
+        $service = Get-Service -ComputerName $addr -Name $service
+        
     }
-    while ($service.State -ne $serviceState)
-
-    Write-Host -foregroundcolor DarkGray -NoNewLine " Service is "
-    Write-Host -foregroundcolor Gray $serviceState
+    while ($service.State -ne $state)
+    Write-Host -Foregroundcolor DarkGray -NoNewLine " Service is "
+    Write-Host -Foregroundcolor Gray $serviceState
 }
 
-#<summary>
-# Starts the SharePoint Timer Service on each server in the SharePoint Farm.
-#</summary>
-#<param name="$farm">The SharePoint farm object.</param>
-function StartSharePointTimerServicesInFarm($farm) {
-    Write-Host ""
 
-    # Iterate through each server in the farm, and each service in each server
-    foreach ($server in $farm) {
-        foreach ($instance in $server.ServiceInstances) {
-            # If the server has the timer service then start the service
-            if ($instance.TypeName -eq $timerServiceInstanceName) {
-                [string]$serverName = $server.Name
-
-                Write-Host -foregroundcolor DarkGray -NoNewline "Start '$timerServiceName' service on server: "
-                Write-Host -foregroundcolor Gray $serverName
-
-                $service = Get-WmiObject -ComputerName $serverName Win32_Service -Filter "Name='$timerServiceName'"
-                sc.exe \\$serverName start $timerServiceName > $null
-
-                WaitForServiceState $serverName $timerServiceName "Running"
-
-                break;
-            }
-        }
-    }
-
-    Write-Host ""
-}
-
-#<summary>
 # Removes all xml files recursive on an UNC path
-#</summary>
-#<param name="$farm">The SharePoint farm object.</param>
-function DeleteXmlFilesFromConfigCache($farm) {
-    Write-Host ""
+function DeleteXmlCache($farm) {
     Write-Host -foregroundcolor DarkGray "Delete xml files"
-
-    [string] $path = ""
 
     # Iterate through each server in the farm, and each service in each server
     foreach ($server in $farm) {
@@ -1577,35 +1538,6 @@ function DeleteXmlFilesFromConfigCache($farm) {
                 $path = "\\" + $serverName + "\c$\ProgramData\Microsoft\SharePoint\Config\*-*\*.xml"
                 Remove-Item -path $path -Force
 
-                break
-            }
-        }
-    }
-
-    Write-Host ""
-}
-
-#<summary>
-# Clears the SharePoint cache on an UNC path
-#</summary>
-#<param name="$farm">The SharePoint farm object.</param>
-function ClearTimerCache($farm) {
-    Write-Host ""
-    Write-Host -foregroundcolor DarkGray "Clear the cache"
-
-    [string] $path = ""
-
-    # Iterate through each server in the farm, and each service in each server
-    foreach ($server in $farm) {
-        foreach ($instance in $server.ServiceInstances) {
-            # If the server has the timer service then force the cache settings to be refreshed
-            if ($instance.TypeName -eq $timerServiceInstanceName) {
-                [string]$serverName = $server.Name
-
-                Write-Host -foregroundcolor DarkGray -NoNewline "Clearing timer cache on server: "
-                Write-Host -foregroundcolor Gray $serverName
-
-                # Clear the cache on an UNC path
                 # 1 = refresh all cache settings
                 $path = "\\" + $serverName + "\c$\ProgramData\Microsoft\SharePoint\Config\*-*\cache.ini"
                 Set-Content -path $path -Value "1"
@@ -1614,7 +1546,6 @@ function ClearTimerCache($farm) {
             }
         }
     }
-    Write-Host ""
 }
 
 function TestRemotePowershell() {
